e31d4339f80272097a0ec2e6662680d8963a179b
cloaked04 <iownpratyush@gmail.com>
2025-12-22 19:23:15 -0500

    Remove vendored bundle; ignore vendor/.bundle

--- vendor/bundle/ruby/3.2.0/gems/jekyll-katex-1.0.0/lib/jekyll-katex/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Jekyll
-  module Katex
-    VERSION = '1.0.0'
-  end
-end
--- vendor/bundle/ruby/3.2.0/gems/jekyll-scholar-7.2.3/lib/jekyll/scholar/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  class Scholar
-    VERSION = '7.2.3'.freeze
-  end
-end
